838d0bc24f5352e955079edad8f91b29f72c88aa
Alejandro Mery <amery@jpi.io>
2021-06-05 04:57:47 +0100

    errors: add WithStack() error wrapper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ errors/stack.go
@@ -0,0 +1,33 @@
+package errors
+
+type StackTracer interface {
+	StackTrace() Stack
+}
+
+type StackError struct {
+	stack Stack
+	err   error
+}
+
+func (s *StackError) Error() string {
+	if s.err != nil {
+		return s.err.Error()
+	} else {
+		return "stacktrace"
+	}
+}
+
+func (s *StackError) StackTrace() Stack {
+	return s.stack
+}
+
+func (s *StackError) Unwrap() error {
+	return s.err
+}
+
+func WithStackTrace(skip int, err error) error {
+	return &StackError{
+		err:   err,
+		stack: BackTrace(1 + skip),
+	}
+}
