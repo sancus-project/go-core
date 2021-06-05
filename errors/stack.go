feb9ab0214b23077fd2d1a00c00901cf45810ec1
Alejandro Mery <amery@jpi.io>
2021-06-05 05:15:35 +0100

    errors: Add initial Recover() and *PanicError
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- errors/stack.go
+++ errors/stack.go
@@ -1,5 +1,9 @@
 package errors
 
+import (
+	"fmt"
+)
+
 type StackTracer interface {
 	StackTrace() Stack
 }
@@ -29,5 +33,54 @@ func WithStackTrace(skip int, err error) error {
 	return &StackError{
 		err:   err,
 		stack: BackTrace(1 + skip),
+	}
+}
+
+type Panic interface {
+	Recovered() interface{}
+}
+
+type PanicError struct {
+	stack Stack
+	rvr   interface{}
+}
+
+func (p *PanicError) Error() string {
+	return fmt.Sprintf("panic: %s", p.rvr)
+}
+
+func (p *PanicError) StackTrace() Stack {
+	return p.stack
+}
+
+func (p *PanicError) Unwrap() error {
+	if err, ok := p.rvr.(error); ok {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (p *PanicError) Recovered() interface{} {
+	return p.rvr
+}
+
+func Recover() *PanicError {
+	if rvr := recover(); rvr == nil {
+		// no error
+		return nil
+	} else if p, ok := rvr.(*PanicError); ok {
+		// pass previous panic along
+		return p
+	} else {
+		// spawn new PanicError
+		return panicError(rvr)
+	}
+}
+
+func panicError(rvr interface{}) *PanicError {
+	return &PanicError{
+		rvr:   rvr,
+		stack: BackTrace(2),
 	}
 }
