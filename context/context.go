837d80efb708cc70bb16fce848892b1a19784d94
Alejandro Mery <amery@jpi.io>
2021-06-14 20:12:12 +0100

    context: keep a list of all registered keys, and add Keys() and Values() for logging
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- context/context.go
+++ context/context.go
@@ -1,8 +1,18 @@
 package context
 
+import (
+	"context"
+	"sync"
+)
+
 type contextKey struct {
 	name string
 }
+
+var (
+	list []*contextKey
+	mu   sync.Mutex
+)
 
 func (k *contextKey) String() string {
 	return "context value " + k.name
@@ -10,5 +20,45 @@ func (k *contextKey) String() string {
 
 // Creates new key for context.WithValue()
 func NewContextKey(name string) *contextKey {
-	return &contextKey{name}
+	mu.Lock()
+	defer mu.Unlock()
+
+	k := &contextKey{name}
+
+	list = append(list, k)
+	return k
+}
+
+func Keys() []string {
+	var v []string
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if l := len(list); l > 0 {
+		v = make([]string, 0, l)
+
+		for i, k := range list {
+			v[i] = k.name
+		}
+	}
+
+	return v
+}
+
+func Values(ctx context.Context) map[string]interface{} {
+	var m map[string]interface{}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if l := len(list); l > 0 {
+		m = make(map[string]interface{}, l)
+
+		for _, k := range list {
+			m[k.name] = ctx.Value(k)
+		}
+	}
+
+	return m
 }
