f1ef44f5e6dcb54b79c786f27b47a54dcd47ade4
Alejandro Mery <amery@jpi.io>
2021-06-19 19:23:56 +0100

    errors: add ErrorStack{} implementation of Validator{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- errors/validator.go
+++ errors/validator.go
@@ -1,5 +1,9 @@
 package errors
 
+import (
+	"strings"
+)
+
 type Validator interface {
 	Ok() bool
 	Error() string
@@ -15,4 +19,38 @@ func AsValidator(err error) (Validator, bool) {
 		err = Unwrap(err)
 	}
 	return nil, false
+}
+
+type ErrorStack struct {
+	errors []error
+}
+
+func (s ErrorStack) Ok() bool {
+	return len(s.errors) == 0
+}
+
+func (s ErrorStack) Error() string {
+	var errors []string
+
+	for _, err := range s.errors {
+		errors = append(errors, err.Error())
+	}
+
+	return strings.Join(errors, "\n")
+}
+
+func (s ErrorStack) Errors() []error {
+	return s.errors
+}
+
+func (s *ErrorStack) AppendError(err error) {
+	if err != nil {
+		s.errors = append(s.errors, err)
+	}
+}
+
+func (s *ErrorStack) AppendErrorf(str string, args ...interface{}) {
+	if len(str) > 0 {
+		s.AppendError(New(str, args...))
+	}
 }
