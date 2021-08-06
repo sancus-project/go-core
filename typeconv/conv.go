89698dd4b4ff31a7599b2e48b974d664aaa80bd0
Alejandro Mery <amery@jpi.io>
2021-08-06 23:39:02 +0100

    typeconv: add helpers to safely convert interface{} to integer types
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ typeconv/conv.go
@@ -0,0 +1,125 @@
+package typeconv
+
+import (
+	"fmt"
+)
+
+const intsize = 32 << (^uint(0) >> 32 & 1) // 32 or 64
+
+func asIntN(n int64, size, bitsize int) (int64, bool) {
+	// caller is trusted to not lie about size
+
+	if size <= bitsize {
+		// same or fewer bits, same signness
+		return n, true
+	}
+
+	cutoff := int64(1 << (bitsize - 1))
+	if cutoff > n && n >= -cutoff {
+		// in range
+		return n, true
+	}
+
+	// out of range
+	return 0, false
+}
+
+func asIntN2(u uint64, size, bitsize int) (int64, bool) {
+	// caller is trusted to not lie about size
+
+	if size < bitsize {
+		// fewer bits, despite different signess, always safe.
+		return int64(u), true
+	}
+
+	cutoff := uint64(1 << (bitsize - 1))
+	if u < cutoff {
+		// in range
+		return int64(u), true
+	}
+
+	// out of range
+	return 0, false
+}
+
+func asUintN(u uint64, size, bitsize int) (uint64, bool) {
+	// caller is trusted to not lie about size
+
+	if size <= bitsize {
+		// same or fewer bits, same signness
+		return u, true
+	}
+
+	cutoff := uint64(1 << bitsize)
+	if u < cutoff {
+		// in range
+		return uint64(u), true
+	}
+
+	// out of range
+	return 0, false
+}
+
+func asUintN2(n int64, size, bitsize int) (uint64, bool) {
+	// caller is trusted to not lie about size
+	if n < 0 {
+		// no negatives on uint
+		return 0, false
+	} else if size <= bitsize {
+		// same or fewer bits and positive, always safe
+		return uint64(n), true
+	}
+
+	cutoff := int64(1 << (bitsize))
+	if n < cutoff {
+		// in range
+		return uint64(n), true
+	}
+
+	// out of range
+	return 0, false
+}
+
+func AsInt(v interface{}) (int, bool) {
+	if n, ok := v.(int); ok {
+		return n, true
+	} else if n, ok := AsIntN(v, intsize); ok {
+		return int(n), true
+	} else {
+		return 0, false
+	}
+}
+
+func AsUint(v interface{}) (uint, bool) {
+	if u, ok := v.(uint); ok {
+		return u, true
+	} else if u, ok := AsUintN(v, intsize); ok {
+		return uint(u), true
+	} else {
+		return 0, false
+	}
+}
+
+func AsString(v interface{}) (string, bool) {
+	if v == nil {
+		return "", false
+	} else if s, ok := v.(string); ok {
+		return s, true
+	} else if w, ok := v.(fmt.Stringer); ok {
+		return w.String(), true
+	} else {
+		s := fmt.Sprintf("%v", v)
+		return s, true
+	}
+}
+
+func AsSlice(v interface{}) ([]interface{}, bool) {
+	switch w := v.(type) {
+	case []interface{}:
+		return w, true
+	case interface{}:
+		return []interface{}{w}, true
+	default:
+		return nil, false
+	}
+}
